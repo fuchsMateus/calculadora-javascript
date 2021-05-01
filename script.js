9fe950c56d8809bbeb98cc261ef21605a46d6e79
Mateus Fuchs <mateus5fuchs@gmail.com>
2021-05-01 16:20:44 -0300

    key detection complete

--- script.js
+++ script.js
@@ -36,6 +36,97 @@ function cState(){
 
 }
 
+function keyDetect(event){
+    if(input.value == ""){
+        c.innerText = "C";
+    }
+    else{
+        c.innerText = "CE";
+    }
+
+    nOPar = 0;
+    nCPar = 0;
+  
+    for (let i = 0; i < tempInput.value.length; i++) {
+        if(tempInput.value.charAt(i) == "("){
+            nOPar++;
+        }
+        else if(tempInput.value.charAt(i) == ")"){
+            nCPar++;
+        }
+    }
+
+    if(nOPar > nCPar){
+        let nn = nOPar - nCPar;
+        oPar.innerHTML = "(<sub>"+nn+"</sub>";
+    }
+    else{
+        oPar.innerHTML = "(";
+    }
+
+    if(event.key == "0" || event.key == "1" || event.key == "2"
+    || event.key == "3" || event.key == "4" || event.key == "5"
+    || event.key == "6" || event.key == "7" || event.key == "8"
+    || event.key == "9"){
+        if(input.value != "0"){
+            input.value += event.key;
+            cState();
+            if(tempInput.value.charAt(tempInput.value.length-1) == "="){
+                tempInput.value = "";
+                input.value = event.key;
+                cState();
+            }
+        }
+    }
+    else if(event.key == "Backspace"){
+        clickDel();
+        cState();
+    }
+    else if(event.key == "="){
+        clickEquals();
+        cState();
+    }
+    else if(event.key == "/"){
+        clickDivision();
+        cState();
+    }
+    else if(event.key == "*"){
+        clickTimes();
+        cState();
+    }
+    else if(event.key == "+"){
+        clickPlus();
+        cState();
+    }
+    else if(event.key == "-"){
+        clickMinus();
+        cState();
+    }
+    else if(event.key == "."){
+        clickDot();
+        cState();
+    }
+    else if(event.key == "("){
+        clickOPar();
+        cState();
+    }
+    else if(event.key == ")"){
+        clickCPar();
+        cState();
+    }
+    else if(event.key == "%"){
+        clickPercent();
+        cState();
+    }
+    else if(event.key == "c"){
+        clickC();
+        cState();
+    }
+
+    console.log(event.key);
+
+}
+
 function clickNum(clicked_id){
     const num = document.getElementById(clicked_id);
     if(input.value != "0"){
@@ -363,7 +454,7 @@ function clickDivision(){
             input.value = "";
         }
         else{
-            tempInput.value += input.value + " * ";
+            tempInput.value += input.value + " / ";
             input.value = "";
         }
     }
